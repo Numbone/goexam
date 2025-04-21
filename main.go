fce502208eef0d4188c20580738c7921888448a0
Nurlybek <nurlybekqwerty@gmail.com>
2025-04-21 10:03:38 +0500

    feat: add post calculation

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -39,7 +40,23 @@ func getCalculations(c echo.Context) error{
 }
 
 func postCalculations(c echo.Context) error{
-	return c.JSON(http.StatusOK, calculations)
+	var req CalculationRequest
+	if err:=c.Bind(&req); err!=nil{
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+
+	result ,err:= calculateExpression(req.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	calc:= Calculation{
+		ID:         uuid.NewString(),
+		Expression: req.Expression,
+		Result:     result,
+	}
+
+	calculations=append(calculations,calc)
+	return c.JSON(http.StatusCreated, calc)
 }
 
 func main() {
